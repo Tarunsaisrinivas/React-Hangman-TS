83b244dee77bd6847387672f0a779b26e74ac85a
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-10-03 07:45:03 +0530

    project setup and random words console

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import words from './wordList.json';
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord";
+import { Keyboard } from "./components/Keyboard";
+const App = () => {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    const randomWord = words[Math.floor(Math.random() * words.length)];
+    return randomWord;
+  });
+  const [guessLetter, setGuessLetter] = useState<string[]>([]);
+  console.log(wordToGuess);
+
+  return (
+    <div className="max-w-2xl flex flex-col gap-2 items-center m-auto">
+      <div className="text-2xl text-center">Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
+};
+
+export default App;
